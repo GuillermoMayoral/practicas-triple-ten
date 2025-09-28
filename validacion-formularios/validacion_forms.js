860d9c6cc50c0357560ad9e07171825447c44278
Mayoral Guillermo <g.mayoral.m@outlook.com>
2025-09-27 19:59:54 -0600

    finish practices

---
+++ validacion-formularios/validacion_forms.js
@@ -0,0 +1,14 @@
+const formElement = document.querySelector(".form");
+const formInput = formElement.querySelector(".form__input");
+
+const showError = (input) => {
+    // añadir la clase de error al elemento de entrada
+};
+
+const hideError = (input) => {
+    // eliminar la clase de error del elemento de entrada
+};
+
+formElement.addEventListener("submit", function (evt) {
+    evt.preventDefault();
+});
